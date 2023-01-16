e3c73629a49dfd5439a486ab2c4ce5f15674f95d
SplattedRabbit <julianstyles@live.de>
2023-01-17 00:55:57 +0100

    * Extended nav bar validation

--- src/test/java/template/pageobjects/components/NavigationBar.java
+++ src/test/java/template/pageobjects/components/NavigationBar.java
@@ -1,17 +1,23 @@
 package template.pageobjects.components;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.xceptance.neodymium.util.Neodymium;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class NavigationBar extends AbstractComponent {
+
+  private String datesLink = "https://test.mandragora-thuringia.de//de/#tourdates";
+
+  private String bandLink = "https://test.mandragora-thuringia.de//de/#band";
+
+  private String shopLink = "https://test.mandragora-thuringia.de/de/shop/";
+
+  private String contactLink = "https://test.mandragora-thuringia.de//de/#contact";
 
   SelenideElement navBar = $(".main-header .navbar");
 
@@ -26,21 +32,25 @@ public class NavigationBar extends AbstractComponent {
     // Validate the menu point 'Termine'
     navBar
         .$("#menu-item-14 a")
-        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.termine")));
+        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.termine")))
+        .shouldHave(attribute("href", datesLink));
 
     // Validate the menu point 'Band'
     navBar
         .$("#menu-item-151 a")
-        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.band")));
+        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.band")))
+        .shouldHave(attribute("href", bandLink));
 
     // Validate the menu point 'Shop'
     navBar
         .$("#menu-item-325 a")
-        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.shop")));
+        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.shop")))
+        .shouldHave(attribute("href", shopLink));
 
     // Validate the menu point 'Kontakte'
     navBar
         .$("#menu-item-152 a")
-        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.kontakt")));
+        .shouldHave(exactText(Neodymium.localizedText("homepage.navbar.kontakt")))
+        .shouldHave(attribute("href", contactLink));
   }
 }
