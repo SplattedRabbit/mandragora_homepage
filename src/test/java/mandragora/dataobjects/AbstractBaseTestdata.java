d39fd54b3dd8dc0a99adeb1833ce2df6aed93f4c
Julian Styles <j.styles@pflegeplatzmanager.de>
2023-07-26 16:28:02 +0200

    * Extended AbstractBaseTestdata.java * Extended package_testdata.json

--- src/test/java/mandragora/dataobjects/AbstractBaseTestdata.java
+++ src/test/java/mandragora/dataobjects/AbstractBaseTestdata.java
@@ -1,4 +1,15 @@
 package mandragora.dataobjects;
 
+import java.util.List;
+import mandragora.dataobjects.shopitems.Shopitem;
+
 public class AbstractBaseTestdata {
+
+  protected Shopitem shopItem;
+
+  private List<Shopitem> shopItems;
+
+  public Shopitem getShopItemByItemname(final String itemname){
+    return shopItems.stream().filter(shopItem -> shopItem.getShopItem().equals(itemname)).findFirst().get();
+  }
 }
