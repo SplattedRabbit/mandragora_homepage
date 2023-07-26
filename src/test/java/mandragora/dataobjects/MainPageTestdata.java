1bb8c637876dd91c841f1dcece67bc0e0d270874
Julian Styles <j.styles@pflegeplatzmanager.de>
2023-07-26 13:28:23 +0200

    * Restructured Datamodell for Shopitems * Added Preview-Validation

--- src/test/java/mandragora/dataobjects/MainPageTestdata.java
+++ src/test/java/mandragora/dataobjects/MainPageTestdata.java
@@ -4,39 +4,30 @@ import java.util.List;
 
 public class MainPageTestdata {
 
-    private String testValue;
+  private List<Bandmember> bandmembers;
 
-    private List<Bandmember> bandmembers;
+  private int numberOfMeadAndGreedImages;
 
-    private int numberOfMeadAndGreedImages;
+  private int numberOfMahlstromImages;
 
-    private int numberOfMahlstromImages;
+  public List<Bandmember> getBandmembers() {
+    return bandmembers;
+  }
 
+  public int getNumberOfMeadAndGreedImages() {
+    return numberOfMeadAndGreedImages;
+  }
 
+  public int getNumberOfMahlstromImages() {
+    return numberOfMahlstromImages;
+  }
 
-    public String getTestValue() {
-        return testValue;
-    }
-
-    public List<Bandmember> getBandmembers() {
-        return bandmembers;
-    }
-
-    public int getNumberOfMeadAndGreedImages() {
-        return numberOfMeadAndGreedImages;
-    }
-
-    public int getNumberOfMahlstromImages() {
-        return numberOfMahlstromImages;
-    }
-
-    @Override
-    public String toString() {
-        return "MainPageTestdata{" +
-            "testValue='" + testValue + '\'' +
-            ", bandmembers=" + bandmembers +
-            ", numberOfMeadAndGreedImages=" + numberOfMeadAndGreedImages +
-            ", numberOfMahlstromImages=" + numberOfMahlstromImages +
-            '}';
-    }
+  @Override
+  public String toString() {
+    return "MainPageTestdata{" +
+        "bandmembers=" + bandmembers +
+        ", numberOfMeadAndGreedImages=" + numberOfMeadAndGreedImages +
+        ", numberOfMahlstromImages=" + numberOfMahlstromImages +
+        '}';
+  }
 }
--- src/test/java/mandragora/pageobjects/components/homepage/Merchandise.java
+++ src/test/java/mandragora/pageobjects/components/homepage/Merchandise.java
@@ -1,10 +1,13 @@
 package mandragora.pageobjects.components.homepage;
 
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.SelenideElement;
+import com.google.gson.JsonObject;
+import com.xceptance.neodymium.util.DataUtils;
 import com.xceptance.neodymium.util.Neodymium;
 import io.qameta.allure.Step;
 import mandragora.pageobjects.components.AbstractComponent;
@@ -25,5 +28,37 @@ public class Merchandise extends AbstractComponent {
   public void validateMerchandise() {
     var merchandisePreviewArticle = merchandiseBlock.$$("li")
         .shouldHave(CollectionCondition.size(3));
+
+    for (SelenideElement currentPreview : merchandisePreviewArticle) {
+      // get the title of the shown shopitem to fetch to corresponding attribute values
+      String currentMerchItemTitle = currentPreview.$(".wc-block-grid__product-title").getText();
+      var currentMerchItem = DataUtils.getDataAsJsonObject().getAsJsonObject("merchPreview")
+          .getAsJsonObject(currentMerchItemTitle);
+
+      // ensure the item has the correct colour
+      currentPreview.$(".wc-block-grid__product-image img").shouldHave(
+          attribute("alt", altAttributeBuilder(currentMerchItemTitle, currentMerchItem)));
+
+      // ensure the itemlink is correct
+      currentPreview.$(".wc-block-grid__product-link").shouldHave(attribute("href",
+          Neodymium.localizedText("homepage.merchandise.shoplink")
+              + currentMerchItemTitle.toLowerCase()
+              + "/"));
+
+      //ensure the correct price is displayed
+      currentPreview.$(".price")
+          .shouldHave(exactText(currentMerchItem.getAsJsonPrimitive("price").getAsString()));
+    }
+  }
+
+  private String altAttributeBuilder(String itemTitle, JsonObject merchItem) {
+    String altValue;
+    if (itemTitle.equals("Krake-Hoodie")) {
+      altValue = itemTitle;
+    } else {
+      altValue = itemTitle.replaceFirst("-", " ") + " "
+          + merchItem.getAsJsonPrimitive("colour").getAsString();
+    }
+    return altValue;
   }
 }
