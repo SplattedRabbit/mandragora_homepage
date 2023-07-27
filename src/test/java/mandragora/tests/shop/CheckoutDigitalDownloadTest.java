142170cda3be92dcbcf01c58fc045a643e54c2e0
Julian Styles <j.styles@pflegeplatzmanager.de>
2023-07-27 15:02:30 +0200

    * Comment

--- src/test/java/mandragora/tests/shop/CheckoutDigitalDownloadTest.java
+++ src/test/java/mandragora/tests/shop/CheckoutDigitalDownloadTest.java
@@ -35,6 +35,6 @@ public class CheckoutDigitalDownloadTest extends AbstractTest {
 
     checkoutPage.acceptTerms();
     checkoutPage.choosePaymentMethod(testdata.getPaymentMethod());
-
+    //comment
   }
 }
